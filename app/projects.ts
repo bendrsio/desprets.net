143b6469411cf8ac1771d8d13fd67a142ab3bf9e
bendsp <benjy.desprets@gmail.com>
2025-05-01 00:45:34 -0400

    add: fundamental mission

--- app/projects.ts
+++ app/projects.ts
@@ -23,6 +23,8 @@ export const projects = [
     # Fundamental
     ## Origin
     I founded Fundamental with 5 classmates in 2024 as part of Epitech's [EIP Program](https://experience.epitech.eu/).
+    ## Mission
+    Our mission is to make crypto accessible to everyone, regardless of their technical background. We believe that crypto has the potential to revolutionize the way we think about money and finance, and we want to be at the forefront of that revolution.
 `,
     images: ["/Fundamental.jpeg"],
   },
